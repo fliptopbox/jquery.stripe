adcd2a55dab4aec73f1db27b89061a340fbcf010
Bruce Thomas <response.write@gmail.com>
2013-05-07 19:43:13 +0100

    initial

---
+++ Gruntfile.js
@@ -0,0 +1,83 @@
+module.exports = function(grunt) {
+
+	// Project configuration.
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('stripe.jquery.json'),
+		/*jshint: {
+			all: ['src/*.js'],
+			options: {
+				browser: true,
+				bitwise: true,
+				eqeqeq: true,
+				passfail: false,
+				nomen: false,
+				plusplus: false,
+				undef: true,
+				evil: true,
+				devel: true
+			},
+			globals: {
+				jQuery: true,
+				Zepto: true,
+				module: true
+			}
+		},*/
+		uglify: {
+			options: {
+				banner: [
+					'/**',
+					'	jQuery Plugin .stripe( options )',
+					'	@version  : <%= pkg.version %>',
+					'	@author   : Bruce Thomas',
+					'	@requires : jquery<%= pkg.dependencies.jquery %>',
+					'	@github   : <%= pkg.docs %>',
+					'*/',
+					''
+				].join('\n')
+			},
+			dist: {
+				src: 'src/jquery.<%= pkg.name %>.js',
+				dest: 'dist/jquery.<%= pkg.name %>.min.js'
+			}
+		},
+		copy: {
+			main: {
+				files: {
+					"dist/" : ['src/**']
+				}
+			}
+		},
+		concat: {
+			options: {
+				stripBanners: true,
+				banner: [
+					'/**',
+					'	jQuery Plugin .stripe( options )',
+					'	@version  : <%= pkg.version %>',
+					'	@author   : Bruce Thomas',
+					'	@requires : jquery<%= pkg.dependencies.jquery %>',
+					'	@github   : <%= pkg.docs %>',
+					'*/',
+					''
+				].join('\n')
+			},
+			dist: {
+				src: 'src/jquery.<%= pkg.name %>.js',
+				dest: 'dist/jquery.<%= pkg.name %>.js'
+			}
+		}
+	});
+
+	//grunt.loadTasks('tasks');
+
+	// Load the plugin that provides the "uglify" task.
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	//grunt.loadNpmTasks('grunt-contrib-jshint');
+
+	// Default task(s).
+	//grunt.registerTask('default', ['jshint', 'concat','uglify']);
+	grunt.registerTask('default', ['copy', 'concat','uglify']);
+
+};
---
+++ README.md
@@ -0,0 +1,17 @@
+# jQuery Plugin .stripe( options ) [Demo](http://fliptopbox.com/demo/slice/)
+    @version  : 1.0
+    @author   : Bruce Thomas
+    @requires : jQuery Core 1.7+
+    @github   : https://github.com/fliptopbox/jquery.stripe/
+
+## Purpose:
+
+A jQuery plug-in to create a cool striped gallery object.
+
+## Usage example:
+    $('.gallery').stripe(); // (default)
+    $('.gallery').stripe({ 'automatic': true, 'buttons': true, 'delay': 5000, 'min-width': 20 });
+
+## Things to note
+
+The height of the gallery is determined by the height of the smallest image. The images are proportionately down scaled to fit, and preserve image quality. You can use a combination of portrait and landscape images. Navigation buttons, and auto animate settings are available, see OPTIONS below. The auto-animation timer is paused if you hover over the gallery or the navigation buttons, and reactivated onMouseout. There is a calculated maximum image width to ensure an image doesn't consume the entire width of the gallery, this width depends on the number of images, and the 'min-width' option.
